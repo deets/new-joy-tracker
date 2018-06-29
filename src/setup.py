457f8e720504a8d9a8f6ed2c1e3d77870303ad40
Diez B. Roggisch <deets@web.de>
2018-06-29 15:51:02 +0200

    wip: report memory usage

--- src/setup.py
+++ src/setup.py
@@ -1,3 +1,4 @@
+import gc
 import array
 import machine
 import time
@@ -97,7 +98,12 @@ def main(name="BOB\0"):
     bmp_data = array.array("i", [0, 0, 0])
     address = (broadcast_address, PORT)
     protocol_buffer = protocol.buffer
+    then = time.time()
     while True:
         pressure_sensor.read_compensated_data(bmp_data)
         protocol.message(bmp_data[0], bmp_data[1])
         s.sendto(protocol_buffer, address)
+        if time.time() - then > 1:
+            print(gc.mem_free())
+            gc.collect()
+            then += 1
