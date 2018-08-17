f933ef97d2e07be1faead303d2659da3e0845a1c
Diez B. Roggisch <deets@web.de>
2018-08-17 18:01:33 +0200

    Just a bit of documentation

--- src/protocol.py
+++ src/protocol.py
@@ -8,6 +8,16 @@ class Protocol:
     FORMAT = "<IHIII" # NAME, SEQUENCE, TIMESTAMP, TEMPERATURE, PRESSURE
 
     def __init__(self, name):
+        """
+        name must be a four-character name that identifies
+        the device. Fill with \0 to pad, e.g
+
+        BOB\0
+        EVE\0
+        ANNA
+        DIEZ
+
+        """
         assert len(name) == 4
         self._name = ustruct.unpack("<I", ustruct.pack("ssss", *name))[0]
         self.buffer = bytearray(ustruct.calcsize(self.FORMAT))
