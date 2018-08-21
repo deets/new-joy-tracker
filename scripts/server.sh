f45d3f74ff3a1a7c86bc92aff8d4bc3cb5feb734
Diez B. Roggisch <deets@web.de>
2018-08-21 11:57:40 +0200

    Refactor server into package

---
+++ scripts/server.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.6
+# -*- code: python -*-
+import os
+import sys
+
+sys.path.append(
+    os.path.join(
+        os.path.dirname(__file__),
+        "..",
+    )
+)
+
+from newjoy.server import main
+main()
