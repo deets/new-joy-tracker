62dde1e284257212be8353b03ec825558f042652
Diez B. Roggisch <deets@web.de>
2018-08-21 10:40:31 +0200

    Integrate visualisation with async server

--- scripts/simple-async-server.py
+++ scripts/simple-async-server.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3.6
 import struct
+import argparse
 
 from socket import socket, SO_REUSEADDR, SOL_SOCKET
 from asyncio import Task, coroutine, get_event_loop
 
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
+
+DEFAULT_UDP_PORT = 5005
+DEFAULT_SERVER_PORT = 5000
 
 class Deriver():
 
@@ -39,13 +43,23 @@ class Peer(object):
                                # ACC_X, ACC_Y, ACC_Z, TEMP, GYR_X, GYR_Y, GYR_Z
                                # CHECKSUM
 
-    def __init__(self, loop, server, sock, name, client):
+    def __init__(
+            self,
+            loop,
+            server,
+            sock,
+            name,
+            client,
+            visualise_callback,
+        ):
         self._loop = loop
         self.name = name
         self._sock = sock
         self._server = server
         self._client = client
+        self._visualise_callback = visualise_callback
         Task(self._peer_loop())
+
 
     async def _peer_loop(self):
         packet_length = struct.calcsize(self.FORMAT)
@@ -87,11 +101,13 @@ class Peer(object):
                 b.add_arg(pressure_d)
                 b.add_arg(pressure_a)
 
-                self._client.send(b.build())
+                message = b.build()
+                self._client.send(message)
+                self._visualise_callback(message)
 
 
 class Server(object):
-    def __init__(self, port, client):
+    def __init__(self, port, client, visualise_callback):
         self._loop = get_event_loop()
         self._serv_sock = socket()
         self._serv_sock.setblocking(0)
@@ -100,6 +116,7 @@ class Server(object):
         self._serv_sock.listen(5)
         self._peers = []
         self._client = client
+        self._visualise_callback = visualise_callback
         Task(self._server())
 
     def remove(self, peer):
@@ -114,7 +131,14 @@ class Server(object):
         while True:
             peer_sock, peer_name = await self._loop.sock_accept(self._serv_sock)
             peer_sock.setblocking(0)
-            peer = Peer(self._loop, self, peer_sock, peer_name, self._client)
+            peer = Peer(
+                self._loop,
+                self,
+                peer_sock,
+                peer_name,
+                self._client,
+                self._visualise_callback
+            )
             print(peer, "created")
             self._peers.append(peer)
 
@@ -124,8 +148,24 @@ class Server(object):
 
 
 def main():
-    client = udp_client.UDPClient('192.168.0.102', 5005)
-    server = Server(5000, client)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("destination", help="Destination UDP IP")
+    parser.add_argument("--port", help="Destination UDP port", default=DEFAULT_UDP_PORT)
+    parser.add_argument("-v", "--visualise", help="Send OSC to visualisation server, the format for this argument is ip:port")
+    opts = parser.parse_args()
+
+    client = udp_client.UDPClient(opts.destination, opts.port)
+
+    def visualise_callback(_):
+        pass
+
+    if opts.visualise is not None:
+        ip, port = opts.visualise.split(":")
+        vis_client = udp_client.UDPClient(ip, int(port))
+        def visualise_callback(message):
+            vis_client.send(message)
+
+    server = Server(DEFAULT_SERVER_PORT, client, visualise_callback)
     server.run_forever()
 
 if __name__ == '__main__':
--- scripts/visualisation.py
+++ scripts/visualisation.py
@@ -1,6 +1,6 @@
 from functools import partial
 from random import random
-from threading import Thread
+import threading
 from datetime import datetime, timedelta
 import time
 
@@ -8,31 +8,52 @@ from bokeh.models import ColumnDataSource
 from bokeh.plotting import curdoc, figure
 
 from tornado import gen
+from pythonosc import osc_server
+from pythonosc import dispatcher
 
-# this must only be modified from a Bokeh session callback
-source = ColumnDataSource(data=dict(x=[datetime.now()], pressure=[0]))
-
-# This is important! Save curdoc() to make sure all threads
-# see the same document.
-doc = curdoc()
+ROLLOVER = 1000
 
 @gen.coroutine
-def update(x, pressure):
-    source.stream(dict(x=[x], pressure=[pressure]), rollover=100)
+def update(source, x, pressure):
+    source.stream(dict(x=[x], pressure=[pressure]), rollover=ROLLOVER)
 
-def blocking_task():
-    while True:
-        # do some blocking computation
-        time.sleep(0.1)
-        y = random()
-        x = datetime.now()
-        # but update the document from callback
-        doc.add_next_tick_callback(partial(update, x=x, pressure=y))
 
-p = figure(x_axis_type='datetime')
-l = p.line(x='x', y='pressure', source=source)
+def schedule_update(doc, source, path, pressure, pressure_d, pressure_a):
+    x = datetime.now()
+    # but update the document from callback
+    doc.add_next_tick_callback(
+        partial(
+            update,
+            source=source,
+            x=x,
+            pressure=pressure,
+        )
+    )
 
-doc.add_root(p)
 
-thread = Thread(target=blocking_task)
-thread.start()
+def main():
+    # this must only be modified from a Bokeh session callback
+    source = ColumnDataSource(data=dict(x=[datetime.now()], pressure=[0]))
+
+    # This is important! Save curdoc() to make sure all threads
+    # see the same document.
+    doc = curdoc()
+
+    p = figure(x_axis_type='datetime')
+    l = p.line(x='x', y='pressure', source=source)
+
+    doc.add_root(p)
+
+    disp = dispatcher.Dispatcher()
+    # This will print all parameters to stdout.
+    disp.map("/filter", partial(schedule_update, doc, source))
+    server = osc_server.BlockingOSCUDPServer(("localhost", 10000), disp)
+    server_thread = threading.Thread(target=server.serve_forever)
+    server_thread.start()
+    #server.shutdown()
+
+    #thread = Thread(target=blocking_task, args=(doc, source))
+    #thread.start()
+
+
+main()
