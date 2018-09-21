8800ba618ce2a59be9872524474f67f7637e87f2
Diez B. Roggisch <deets@web.de>
2018-09-21 15:51:01 +0200

    Generated manufacturing files and ordered PCB

---
+++ new-joy-board/scripts/zip-manufacturing-files.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- mode: python -*-
+import os
+import sys
+import zipfile
+import glob
+
+BASE = os.path.join(os.path.dirname(__file__), "..")
+GLOBS = "*.gbr", "*.drl"
+
+def main():
+    os.chdir(BASE)
+    with zipfile.ZipFile("new-joy-board.zip", "w") as outf:
+        for g in GLOBS:
+            for name in glob.glob(g):
+                outf.write(name)
+
+
+if __name__ == '__main__':
+    main()
