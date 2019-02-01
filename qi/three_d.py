a8d9c0e6d74389a556e2976993f7e55b09f00ef5
Diez B. Roggisch <deets@web.de>
2019-02-01 14:54:31 +0100

    Replaced cylinder with coordinate root

--- qi/three_d.py
+++ qi/three_d.py
@@ -1,35 +1,21 @@
-from pyqtgraph.Qt import QtCore
+from pyqtgraph.Qt import QtCore, QtGui
 import pyqtgraph.opengl as gl
-import numpy as np
 
 
 class QuaternionRep(QtCore.QObject):
-    LENGTH = 3
+    SIZE = QtGui.QVector3D(3, 3, 3)
+    # z is green
+    # y is yellow
+    # x is blue
 
     def __init__(self, w):
         super().__init__()
-
-        md = gl.MeshData.cylinder(
-            rows=10,
-            cols=20,
-            radius=[.5, .5],
-            length=self.LENGTH
-        )
-        colors = np.ones((md.faceCount(), 4), dtype=float)
-        colors[::2, 0] = 0
-        colors[:, 1] = np.linspace(0, 1, colors.shape[0])
-        md.setFaceColors(colors)
-        self._cylinder = gl.GLMeshItem(
-            meshdata=md,
-            smooth=True,
-            drawEdges=True,
-            edgeColor=(1, 0, 0, 1),
-            shader='balloon',
-        )
+        self._cylinder = gl.GLAxisItem(size=self.SIZE)
         w.addItem(self._cylinder)
+        self.update(QtGui.QQuaternion())
 
     def update(self, quat):
         c = self._cylinder
         c.resetTransform()
         c.transform().rotate(quat)
-        c.transform().translate(0, 0, -self.LENGTH / 2)
+        c.transform().translate(0, 0, 1)
