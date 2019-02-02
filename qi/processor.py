b352824a299e19a86e80fe432b6231a169e1ffff
Diez B. Roggisch <deets@web.de>
2019-02-02 13:51:19 +0100

    Emit signal with subsequent angle

--- qi/processor.py
+++ qi/processor.py
@@ -6,7 +6,19 @@ from pyqtgraph.Qt import QtCore
 from .common import angle_between_quats
 
 
+def angle_between_succesors(args):
+    old, new = args
+    return old[0], angle_between_quats(
+        old[1], new[1],
+    )
+
+
 class QuaternionProcessor(QtCore.QObject):
+
+    subsequent_angle = QtCore.pyqtSignal(
+        int, float,
+        name="subsequent_angle"
+    )
 
     def __init__(self):
         super().__init__()
@@ -15,7 +27,12 @@ class QuaternionProcessor(QtCore.QObject):
     def update(self, sensor_no, quat):
         if sensor_no not in self._subjects:
             s = self._subjects[sensor_no] = rx.subjects.Subject()
-            s.pairwise().map(lambda qs: angle_between_quats(*qs)).subscribe(
-                lambda value: print("Angle: {0}".format(value)))
+            subsequent_angle = s.pairwise().map(angle_between_succesors)
+            subsequent_angle.subscribe(
+                lambda value: print("Angle: {0}".format(value))
+            )
+            subsequent_angle.subscribe(
+                lambda arg: self.subsequent_angle.emit(arg[0], arg[1])
+            )
 
-        self._subjects[sensor_no].on_next(quat)
+        self._subjects[sensor_no].on_next((sensor_no, quat))
